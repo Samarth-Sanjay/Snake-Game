c410995e60079a3d47a4d2178a58c8bd0b546911
Samarth Sanjay <samarth.sanjay5@gmail.com>
2024-03-26 15:54:16 -0700

    added body to the snake

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+segments = []
+
+starting_positions = [(0,0), (-20,0), (-40,0)]
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+game_is_on = True
+while(game_is_on):
+    for seg in segments:
+        seg.forward(20)
+
 screen.exitonclick()
